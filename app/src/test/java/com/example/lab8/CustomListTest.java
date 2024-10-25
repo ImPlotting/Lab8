698deda8d47308c2aee10e6c8495b98e0ed317ed
Ayub Ali <83373695+ImPlotting@users.noreply.github.com>
2024-10-25 15:28:34 -0600

    Implemented hasCity method in CustomList

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,16 +1,29 @@
 package com.example.lab8;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-//import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
+
+    @BeforeEach
+    public void setup() {
+        list = new CustomList(null, new ArrayList<City>());
+    }
+
+    @Test
+    public void testHasCity() {
+        City city = new City("Edmonton", "Alberta");
+        assertFalse(list.hasCity(city)); // Initially, the city is not in the list
+        list.addCity(city);
+        assertTrue(list.hasCity(city));  // After adding, the city should be in the list
+    }
 
 }
